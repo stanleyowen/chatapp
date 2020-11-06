6af1074cbdbb2196923853e1a4e146c73ead4d9b
stanleyowen <69080584+stanleyowen@users.noreply.github.com>
2020-11-06 16:44:31 +0700

    Create Function

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
 const PORT = process.env.PORT || 5000;
 const router = require('./router');
@@ -10,7 +11,23 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-    console.log('A new User connected!');
+    socket.on('join', ({ name, room }, callback) => {
+        const { error, user } = addUser({ id: socket.id, name, room });
+        if(error) { return callback(error); };
+        socket.join(user.room);
+
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}` });
+        socket.broadcast.to(ser.room).emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}` })
+
+        callback();
+
+        socket.on('sendMessage', ({ message, callback }) => {
+            const user = getUser(socket.id);
+            io.to(user.room).emit('message', { user: user.name, text: message });
+            callback();
+        })
+    });
+
     socket.on('disconnect' , () => {
         console.log('A user had left!');
     })
---
+++ server/users.js
@@ -0,0 +1,26 @@
+const users = [];
+
+const addUser = ({ id, name, room }) => {
+    name = name.trim().toLowerCase();
+    room = name.trim().toLowerCase();
+    const existingUser = user.find((user) => user.name === name && user.room === room);
+    if(existingUser) {
+        return { error: 'Username is Taken, Please Try Another Username' }
+    }
+    const user = { id, name, room };
+    users.push(user);
+    return { user }
+}
+
+const removeUser = ({ id }) => {
+    const index = users.findIndex((user) => user.id === id)
+    if(index !== -1){
+        return users.splice(index, 1)[0];
+    }
+}
+
+const getUser = (id) => users.find((user) => user.id == id);
+
+const getUsersInRoom = (room) => users.filter((user) => user.room === room);
+
+module.exports = { addUser, removeUser, getUser, getUsersInRoom };
